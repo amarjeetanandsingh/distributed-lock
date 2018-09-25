e83f27dae58db6096506e041477ce0b01cc625d9
Amarjeet Anand <amarjeetanandsingh@gmail.com>
2018-09-25 16:00:02 +0530

    initiated as maven project

---
+++ src/main/java/in/amarjeet/lock/App.java
@@ -0,0 +1,11 @@
+package in.amarjeet.lock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+	public static void main(String[] args) {
+		List<String> str = new ArrayList<>();
+		str.forEach(string -> System.out.println(string));
+	}
+}
